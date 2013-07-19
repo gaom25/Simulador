afd2c5e25718c9f282845225adc411697241b11d
Gustavo Ortega <ovasgus2508@gmail.com>
2013-07-18 20:58:53 -0430

    la vaina de krys que ahora es mia XD

--- Repositorio/commit.java
+++ Repositorio/commit.java
@@ -3,6 +3,13 @@ import java.net.MalformedURLException;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Date;
+import java.lang.reflect.Array;
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Locale;
 
 /**
  *
@@ -58,24 +65,38 @@ public class commit {
 //             PublicKey llave = serer.getPublica();
 //             File temporal = new(File("./temporalEncriptado"));  
         File[] listOfFiles = f1.listFiles(); 
+        ArrayList<File> archivitos = new ArrayList<File>();
+
+
+
+
 //      String tmp = "";
         
         for (int i = 0; i < listOfFiles.length; i++){
       
-//        if (listOfFiles[i].isFile()){
-//          tmp = listOfFiles[i].getName();
-//          Cifrado.cifrado(new FileInputStream(tmp), new FileOutputStream(temporal+"/"+tmp),llave);
-//        }
-//      }
+            try {
+                String nombreArchivo = listOfFiles[i].getName();
+                File inFile = new File(nombreArchivo);
+                File outFile = new File(nombreArchivo);
 
-//      listOfFiles = temporal.listFiles(); 
-//      for (int i = 0; i < listOfFiles.length; i++){
-          if (listOfFiles[i].isFile()){
-            archivos.add(listOfFiles[i]);
-          }
+                FileInputStream in = new FileInputStream(inFile);
+                FileOutputStream out = new FileOutputStream(outFile);
+
+                int c;
+                while( (c = in.read() ) != -1)
+                    out.write(c);
+
+                in.close();
+                out.close();
+                archivitos.add(outFile);
+            } catch(IOException e) {
+                System.err.println("Hubo un error de entrada/salida!!!");
+            }
+
+            
         }
         
-            Actualizacion a = new Actualizacion("commit",nombre,repo,archivos,hora);
+            Actualizacion a = new Actualizacion("commit",nombre,repo,archivitos,hora);
           
             /**Luego buscamos el servicio como tal*/
             Acciones c = (Acciones) Naming.lookup("rmi://" + serv.getHost() + ":" + 55555 + "/REPO");
